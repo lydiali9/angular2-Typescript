2befbccd233d43c39f326fdd5efef317e44d6529
lynnli <li.y.lynn@pwc.com>
2017-07-07 16:15:42 +0800

    [Feature]: add express

--- angular2-Typescript/src/app/app.component.ts
+++ angular2-Typescript/src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -7,5 +25,12 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-   title = 'My first Angular App!';
+    title = 'Tour of Heroes';
+    heroes = HEROES;
+    selectedHero: Hero;
+ 
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
+
