adbbf0dca8b5ba703dbf03843a38271232b9e25b
lynnli <li.y.lynn@pwc.com>
2017-07-07 17:21:41 +0800

    [Feature]: finish my first page

--- angular2-Typescript/src/app/app.component.ts
+++ angular2-Typescript/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
--- angular2-Typescript/src/app/app.module.ts
+++ angular2-Typescript/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeroDetailComponent
     ],
     imports: [
         BrowserModule,
