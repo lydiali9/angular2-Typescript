eb445222120f8e2e271f4a2b96171629ad6c6684
lynnli <li.y.lynn@pwc.com>
2017-07-10 11:11:52 +0800

    [Feature]: create a hero Service

--- angular2-Typescript/src/app/app.component.ts
+++ angular2-Typescript/src/app/app.component.ts
@@ -1,32 +1,31 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    providers: [HeroService]
 })
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
- 
+
+    constructor(private heroService: HeroService) { }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+     
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+     
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
 }
-
